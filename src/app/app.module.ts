1a77a3304242ca3a737ec3fadbd90b6c546ef3e1
LukasKatona <katona.lukasek@gmail.com>
2019-12-01 18:07:46 +0100

    video-box unfinished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { MoreVideosComponent } from './more-videos/more-videos.component';
+import { VideoBoxComponent } from './video-box/video-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HeaderComponent,
-    MoreVideosComponent
+    MoreVideosComponent,
+    VideoBoxComponent
   ],
   imports: [
     BrowserModule,
