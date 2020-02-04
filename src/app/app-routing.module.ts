8383be84ff8ab2d9a216268f7a73a6e31636208d
abs3nce <vodrazka.jakub@gmail.com>
2020-02-04 14:52:11 +0100

    - about page works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AboutComponent } from './about/about.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "products", component: ProductListComponent },
+  { path: "about", component: AboutPageComponent },
   { path: "", redirectTo: '/home', pathMatch: 'full' },
-  { path: "**", redirectTo: '/home', pathMatch: 'full' }
+  { path: "**", redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
